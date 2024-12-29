14b1018893429cc6f3a72100e953afa38d357bbf
ritik-hedau18 <ritikhedau253@gmail.com>
2024-12-29 18:15:00 +0530

    feat: write rest controller endpoints and request mapping logic

---
+++ backend/src/main/java/com/ritikhedau/flux/FluxApplication.java
@@ -0,0 +1,11 @@
+package com.ritikhedau.flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FluxApplication.class, args);
+    }
+}
